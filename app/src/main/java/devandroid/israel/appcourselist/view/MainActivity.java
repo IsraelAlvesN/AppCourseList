9076f13bf2861e4107bacdecf9a8cce1342606c1
IsraelAlvesN <devhardertresd@gmail.com>
2024-10-03 20:49:43 -0300

    Spinner adapter created

--- app/src/main/java/devandroid/israel/appcourselist/view/MainActivity.java
+++ app/src/main/java/devandroid/israel/appcourselist/view/MainActivity.java
@@ -1,10 +1,11 @@
 package devandroid.israel.appcourselist.view;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,13 +19,12 @@ import java.util.List;
 import devandroid.israel.appcourselist.R;
 import devandroid.israel.appcourselist.controller.CourseController;
 import devandroid.israel.appcourselist.controller.PersonController;
-import devandroid.israel.appcourselist.model.Course;
 import devandroid.israel.appcourselist.model.Person;
 
 public class MainActivity extends AppCompatActivity {
 
     Person person;
-    List<Course> coursesList;
+    List<String> courseNames;
 
     PersonController personController;
     CourseController courseController;
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     Button btClear;
     Button btSave;
     Button btSubmit;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         personController.search(person);
 
         courseController = new CourseController();
-        coursesList = courseController.getCoursesList();
+        courseNames = courseController.dataToSpinner();
 
         etFirstName = findViewById(R.id.ptFirstName);
         etLastname = findViewById(R.id.ptLastname);
@@ -57,11 +58,21 @@ public class MainActivity extends AppCompatActivity {
         btClear = findViewById(R.id.btClear);
         btSave = findViewById(R.id.btSave);
         btSubmit = findViewById(R.id.btSubmit);
+        spinner = findViewById(R.id.spinner);
 
         etFirstName.setText(person.getFirstName());
         etLastname.setText(person.getLastName());
         etCourseName.setText(person.getCourseName());
         etPhone.setText(person.getPhoneNumber());
+
+        //Adapter
+        //Layout
+        //Injetar adapter ao Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courseController.dataToSpinner());
+
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        spinner.setAdapter(adapter);
 
         btClear.setOnClickListener(new View.OnClickListener() {
             @Override
